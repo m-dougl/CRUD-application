70d6e68a185567bbc1e7e3bedde24574ae4fd35e
m-dougl <douglas.maia.santos@itec.ufpa.br>
2024-05-08 20:59:17 -0300

    Request model

--- models/foods.py
+++ models/foods.py
@@ -1,3 +1,11 @@
-class Foods:
-    def __init__(self):
-        pass 
+class RequestModel:
+    def __init__(self, 
+                 burguer_choice,
+                 burguer_quantity,
+                 soup_choice,
+                 soup_quantity):
+        self.burguer_choice = burguer_choice if burguer_choice !=0 else None
+        self.burguer_quantity = burguer_quantity
+        self.soup_choice = soup_choice if soup_choice !=0 else None
+        self.soup_quantity = soup_quantity
+        
