3be1f4ce8061f1cfc942cf83b26152a97f2cf685
m-dougl <douglas.maia.santos@itec.ufpa.br>
2024-05-08 22:44:25 -0300

    fixed request model

--- models/foods.py
+++ models/foods.py
@@ -14,12 +14,12 @@ class RequestModel:
         self.last_name = last_name
         self.address = address
         self.date = date
-        
+
         self.burguer_choice = burguer_choice if burguer_choice != 0 else None
         self.burguer_quantity = burguer_quantity
         self.soup_choice = soup_choice if soup_choice != 0 else None
         self.soup_quantity = soup_quantity
         self.requests = f"""
-            {self.burguer_quantity}x{self.burguer_choice}
-            {self.soup_choice}x{self.soup_choice}
+            {self.burguer_quantity} x {self.burguer_choice}
+            {self.soup_choice} x {self.soup_choice}
         """
