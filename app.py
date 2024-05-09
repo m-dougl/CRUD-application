11e3e0a4f3f76914a25875bd53176d07b6ee6bd5
m-dougl <douglas.maia.santos@itec.ufpa.br>
2024-05-08 23:38:26 -0300

    general update

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-import streamlit as st 
+import streamlit as st
 from st_pages import Page, show_pages
 
+
 def app():
-    st.set_page_config(page_title="Foods delivery", 
-                       layout="wide", 
-                       initial_sidebar_state="expanded")
-    
+    st.set_page_config(
+        page_title="Foods delivery", layout="wide", initial_sidebar_state="expanded"
+    )
+
     show_pages(
         [
             Page("Pages/main_menu.py", "Place your order", "🍔"),
             Page("Pages/consult.py", "Consult orders", "🛒"),
         ]
     )
-    
+
+
 if __name__ == "__main__":
     app()
