37bfdcc6ea27d6faa5bfcfdc7b50a3af7c525fff
m-dougl <douglas.maia.santos@itec.ufpa.br>
2024-05-09 01:03:22 -0300

    Updated widgets

--- Pages/consult.py
+++ Pages/consult.py
@@ -3,24 +3,27 @@ from services.crud import CRUD
 
 
 def main():
+    st.title("Order list")
     dataframe_expander = st.expander(label="Orders in progress", expanded=True)
     df = CRUD().read()
     dataframe_expander.dataframe(df, hide_index=True, use_container_width=True)
 
-    cols = st.columns((5,5), gap="medium")
+    cols = st.columns((2.5, 5), gap="medium")
     with cols[0]:
         st.write("❌ Cancel your order ")
         first_name = st.text_input("First Name")
         last_name = st.text_input("Last Name")
         cancel_button = st.button("Cancel")
-        
+
         if cancel_button:
-            if all([first_name, last_name]):
+            if all([first_name.capitalize(), last_name.capitalize()]):
                 CRUD().delete(first_name=first_name, last_name=last_name)
                 st.success("✅️ Your order has been cancelled")
             else:
                 st.error("⚠️ Please fill in all the required fields")
             st.experimental_rerun()
-                
+
+
 if __name__ == "__main__":
+    st.set_page_config(page_title="Foods Delivery", layout="wide")
     main()
