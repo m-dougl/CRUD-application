8f2fb12e220c5750bc2f0cc00310e5ad54531644
m-dougl <douglas.maia.santos@itec.ufpa.br>
2024-05-08 22:43:55 -0300

    fixed SQL command to insert data

--- services/crud.py
+++ services/crud.py
@@ -2,47 +2,53 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
+
 class CRUD:
+    def __init__(self):
+        load_dotenv()
+
     def create(self):
-        with psycopg2.connect(host=os.getenv("DB_HOST"),
-                              port=os.getenv("DB_PORT"),
-                              dbname=os.getenv("DB_NAME"),
-                              user=os.getenv("DB_USER"),
-                              password=os.getenv("DB_PASSWORD")) as connection:
+        with psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+        ) as connection:
             cursor = connection.cursor()
             query = """
                     CREATE TABLE IF NOT EXISTS foodsrequests(
-                        id INTEGER PRIMARY KEY,
+                        id SERIAL PRIMARY KEY,
                         first_name VARCHAR(20) NOT NULL,
                         last_name VARCHAR(20) NOT NULL,
-                        addresss TEXT NOT NULL,
+                        address TEXT NOT NULL,
                         date DATE NOT NULL,
-                        requests TEXT NOT NULL
+                        requests TEXT
                     )
             """
             cursor.execute(query)
-        
-    def add(self, 
-            first_name,
-            last_name,
-            address,
-            date,
-            burguer_choice, 
-            burguer_quantity, 
-            soup_choice, 
-            soup_quantity):
-        with psycopg2.connect(host=os.getenv("DB_HOST"),
-                              port=os.getenv("DB_PORT"),
-                              dbname=os.getenv("DB_NAME"),
-                              user=os.getenv("DB_USER"),
-                              password=os.getenv("DB_PASSWORD")) as connection:
+
+    def add(self, request):
+        with psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+        ) as connection:
             cursor = connection.cursor()
             query = """
                     INSERT INTO foodsrequests
                         (first_name, last_name, address, date, requests)
-                    VALUES ?, ?, ?, ?, ?
+                    VALUES (%s, %s, %s, %s, %s)
             """
-                
-                
-if __name__ == "__main__":
-    CRUD().create()
+            cursor.execute(
+                query,
+                (
+                    request.first_name,
+                    request.last_name,
+                    request.address,
+                    request.date,
+                    request.requests,
+                )
+            )
