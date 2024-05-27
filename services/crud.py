5b7a36b2a5af3bd951b37a9c754d0deed61f39de
m-dougl <douglas.maia.santos@itec.ufpa.br>
2024-05-27 20:08:01 -0300

    Updated database connection method

--- services/crud.py
+++ services/crud.py
@@ -2,122 +2,119 @@ import psycopg2
 import os
 import pandas as pd
 from dotenv import load_dotenv
+from models.foods import RequestModel
+from datetime import datetime
 
 
 class CRUD:
     def __init__(self):
         load_dotenv()
+        self.database_parameters = {
+            "host": os.getenv("DB_HOST"),
+            "port": os.getenv("DB_PORT"),
+            "dbname": os.getenv("DB_NAME"),
+            "user": os.getenv("DB_USER"),
+            "password": os.getenv("DB_PASSWORD"),
+        }
+
+    def _get_conection(self):
+        return psycopg2.connect(**self.database_parameters)
 
     def create(self):
-        with psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-        ) as connection:
-            cursor = connection.cursor()
-            query = """
-                    CREATE TABLE IF NOT EXISTS foodsrequests(
-                        id SERIAL PRIMARY KEY,
-                        first_name VARCHAR(20) NOT NULL,
-                        last_name VARCHAR(20) NOT NULL,
-                        address TEXT NOT NULL,
-                        date DATE NOT NULL,
-                        requests TEXT
-                    )
-            """
-            cursor.execute(query)
+        try:
+            with self._get_conection() as connection:
+                cursor = connection.cursor()
+                query = """
+                        CREATE TABLE IF NOT EXISTS foodsrequests(
+                            id SERIAL PRIMARY KEY,
+                            first_name VARCHAR(20) NOT NULL,
+                            last_name VARCHAR(20) NOT NULL,
+                            address TEXT NOT NULL,
+                            date DATE NOT NULL,
+                            requests TEXT
+                        )
+                """
+                cursor.execute(query)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-    def insert(self, request):
-        with psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-        ) as connection:
-            cursor = connection.cursor()
-            query = """
-                    INSERT INTO foodsrequests
-                        (first_name, last_name, address, date, requests)
-                    VALUES (%s, %s, %s, %s, %s)
-            """
-            cursor.execute(
-                query,
-                (
-                    request.first_name,
-                    request.last_name,
-                    request.address,
-                    request.date,
-                    request.requests,
-                ),
-            )
+    def insert(self, request_model):
+        try:
+            with self._get_conection() as connection:
+                cursor = connection.cursor()
+                query = """
+                        INSERT INTO foodsrequests
+                            (first_name, last_name, address, date, requests)
+                        VALUES (%s, %s, %s, %s, %s)
+                """
+                cursor.execute(
+                    query,
+                    (
+                        request_model.first_name,
+                        request_model.last_name,
+                        request_model.address,
+                        request_model.date,
+                        request_model.requests,
+                    ),
+                )
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def read(self):
-        with psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-        ) as connection:
-            query = """
-                    SELECT * FROM foodsrequests
-            """
-            table = pd.read_sql_query(query, connection)
-            return table
-
-    def update(self, new_values, first_name, last_name):
-        with psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-        ) as connection:
-            cursor = connection.cursor()
-            query = """
-                    UPDATE foodsrequests
-                    SET requests = %s
-                    WHERE first_name = %s AND last_name = %s
+        try:
+            with self._get_conection() as connection:
+                query = """
+                        SELECT * FROM foodsrequests
                 """
-            cursor.execute(query, (new_values, first_name, last_name))
+                table = pd.read_sql_query(query, connection)
+                return table
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def update(self, request_model):
+        try:
+            with self._get_conection() as connection:
+                cursor = connection.cursor()
+                query = """
+                        UPDATE foodsrequests
+                        SET requests = CONCAT(requests, %s)
+                        WHERE first_name = %s AND last_name = %s
+                    """
+                cursor.execute(
+                    query,
+                    (
+                        request_model.requests,
+                        request_model.first_name,
+                        request_model.last_name,
+                    ),
+                )
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def delete(self, first_name, last_name):
-        with psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-        ) as connection:
-            cursor = connection.cursor()
-            query = """
-                    DELETE FROM foodsrequests
-                    WHERE first_name=%s AND last_name=%s
-            """
-            cursor.execute(query, (first_name, last_name))
+        try:
+            with self._get_conection() as connection:
+                cursor = connection.cursor()
+                query = """
+                        DELETE FROM foodsrequests
+                        WHERE first_name=%s AND last_name=%s
+                """
+                cursor.execute(query, (first_name, last_name))
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def check_existence(self, first_name, last_name):
-        with psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-        ) as connection:
-            cursor = connection.cursor()
-            query = """
-                    SELECT first_name, last_name FROM foodsrequests
-                    WHERE first_name=%s AND last_name=%s
-            """
-            cursor.execute(query, (first_name, last_name))
-            if cursor.fetchone():
-                return True
-            else:
-                return False
-
-
-if __name__ == "__main__":
-    print(CRUD().check_existence("douglas", "maia"))
+        try:
+            with self._get_conection() as connection:
+                cursor = connection.cursor()
+                query = """
+                        SELECT first_name, last_name FROM foodsrequests
+                        WHERE first_name=%s AND last_name=%s
+                """
+                cursor.execute(query, (first_name, last_name))
+                if cursor.fetchone():
+                    return True
+                else:
+                    return False
+        except Exception as e:
+            print(f"An error occurred: {e}")
